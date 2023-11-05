f4a9be71ea8789a79d394003cd24a075b6b3733b
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-05 01:52:59 -0700

    Update install.sh

--- bootloader/install.sh
+++ bootloader/install.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-# installs grub on legacy boot systems
-# runs from outside the installation
-
-if iroot nobootloader; then
-    echo "skipping grub install"
-    exit
-fi
-
-echo "installing grub for legacy bios"
-DISK="$(iroot grubdisk)"
-
-grub-install "${DISK}"
